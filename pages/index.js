cd104740faea2e7aaaedf90133511cfb42f6fa34
handi7 <handi7.co@gmail.com>
2023-02-26 14:11:27 +0700

    add guess & check

--- pages/index.js
+++ pages/index.js
@@ -35,105 +35,107 @@ export default function Home() {
   };
 
   return (
-    <div className="p-3">
-      <div className="text-center">
-        <h5>Kumpulan kosakata dari standard text book tahun 2015 EPS TOPIK</h5>
-      </div>
-      {data?.loading ? (
-        <div
-          className={
-            "d-flex flext-column justify-content-center align-items-center mt-5 pt-5"
-          }
-        >
-          <Spinner color="light" />
-          <span className="text-white">Sedang mencari. . .</span>
-        </div>
-      ) : search?.word ? (
-        data?.words?.length > 0 ? (
-          <div>
-            <p>
-              Hasil pencarian {search?.type} dengan kata kunci &quot;
-              {search?.word}&quot;.
-            </p>
+    <Row>
+      <Col xs={12}>
+        {data?.loading ? (
+          <div
+            className={
+              "d-flex flext-column justify-content-center align-items-center mt-5 pt-5"
+            }
+          >
+            <Spinner color="light" />
+            <span className="text-white">Sedang mencari. . .</span>
+          </div>
+        ) : search?.word ? (
+          data?.words?.length > 0 ? (
+            <div>
+              <p>
+                Hasil pencarian {search?.type} dengan kata kunci &quot;
+                {search?.word}&quot;.
+              </p>
 
-            <Row>
-              {data?.words?.map((item, idx) => {
-                return item?.kor ? (
-                  <Col key={idx} xs={12} sm={6} lg={4} className="p-1">
-                    <div className="border rounded p-2">
-                      <Item word={item?.kor} />
-                      <Item word={item?.ind} />
-                    </div>
-                  </Col>
-                ) : (
-                  <Col key={idx} lg={12} xl={6} className="bg-transparent">
-                    <Row className="border rounded p-2 my-2 mx-1">
-                      {/* <div className="border rounded"> */}
-                      <Col xs={6} className="bg-transparent text-center">
-                        <Item word={item?.kor1} />
-                        <Item word={item?.ind1} />
-                      </Col>
-                      <Col xs={6} className="bg-transparent text-center">
-                        <Item word={item?.kor2} />
-                        <Item word={item?.ind2} />
-                      </Col>
-                      {/* </div> */}
-                    </Row>
-                  </Col>
-                );
+              <Row>
+                {data?.words?.map((item, idx) => {
+                  return item?.kor ? (
+                    <Col key={idx} xs={12} sm={6} lg={4} className="p-1">
+                      <div className="border rounded p-2">
+                        <Item word={item?.kor} />
+                        <Item word={item?.ind} />
+                      </div>
+                    </Col>
+                  ) : (
+                    <Col key={idx} lg={12} xl={6} className="bg-transparent">
+                      <Row className="border rounded p-2 my-2 mx-1">
+                        {/* <div className="border rounded"> */}
+                        <Col xs={6} className="bg-transparent text-center">
+                          <Item word={item?.kor1} />
+                          <Item word={item?.ind1} />
+                        </Col>
+                        <Col xs={6} className="bg-transparent text-center">
+                          <Item word={item?.kor2} />
+                          <Item word={item?.ind2} />
+                        </Col>
+                        {/* </div> */}
+                      </Row>
+                    </Col>
+                  );
 
-                {
-                  /* {(idx + 1) % 10 === 0 ? (
+                  {
+                    /* {(idx + 1) % 10 === 0 ? (
                             <Adsense
                               adClient="ca-pub-2941796843013407"
                               adSlot="5327133669"
                             />
                           ) : null} */
-                }
-              })}
-            </Row>
-          </div>
+                  }
+                })}
+              </Row>
+            </div>
+          ) : (
+            <div className="text-center">
+              <p>
+                Tidak ditemukan hasil pencarian dengan kata kunci &quot;
+                {search?.word}&quot;.
+              </p>
+            </div>
+          )
         ) : (
-          <div className="text-center">
-            <p>
-              Tidak ditemukan hasil pencarian dengan kata kunci &quot;
-              {search?.word}&quot;.
-            </p>
-          </div>
-        )
-      ) : (
-        <div className="text-center mt-5 px-5">
-          <p>hasil pencarian akan ditampilkan di sini.</p>
+          <div className="text-center mt-5 px-5">
+            <h4>
+              Kumpulan kosakata dari standard text book tahun 2015 EPS TOPIK
+            </h4>
+            <p>hasil pencarian akan ditampilkan di sini.</p>
 
-          {/* <GoogleAdsense
+            {/* <GoogleAdsense
                   client="ca-pub-2941796843013407"
                   slot="5327133669"
                   responsive="true"
                 /> */}
 
-          <Adsense adClient="ca-pub-2941796843013407" adSlot="5327133669" />
+            <Adsense adClient="ca-pub-2941796843013407" adSlot="5327133669" />
 
-          <div className="mt-5 pt-5">
-            <h5>Tersedia juga dalam bentuk android app.</h5>
-            <div>
-              <Image src="/brand.png" width={50} height={50} alt="brand" />
+            <div className="mt-5 pt-5">
+              <h5>Tersedia juga dalam bentuk android app.</h5>
+              <div>
+                <Image src="/brand.png" width={50} height={50} alt="brand" />
+              </div>
+              <a
+                rel="noreferrer"
+                href="https://play.google.com/store/apps/details?id=com.handev.krid15"
+                target="_blank"
+              >
+                Download disini.
+              </a>
             </div>
-            <a
-              rel="noreferrer"
-              href="https://play.google.com/store/apps/details?id=com.handev.krid15"
-              target="_blank"
-            >
-              Download disini.
-            </a>
-          </div>
 
-          <GoogleAdsense
-            client="ca-pub-2941796843013407"
-            slot="5327133669"
-            responsive="true"
-          />
-        </div>
-      )}
-    </div>
+            <GoogleAdsense
+              client="ca-pub-2941796843013407"
+              slot="5327133669"
+              responsive="true"
+            />
+          </div>
+        )}
+      </Col>
+    </Row>
   );
 }
